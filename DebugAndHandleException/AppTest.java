f50f9d8c4ac227bc8bb48f94b85d7ac33ee23730
RATNESH MAURYA <ratneshmaurya2001@gmail.com>
2023-12-30 13:05:04 +0530

    Debug and Handle Exceptions Test files for invalid data

---
+++ DebugAndHandleException/AppTest.java
@@ -0,0 +1,12 @@
+package ApplyOops;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.function.Executable;
+
+class AppTest {
+    @Test void appHasAGreeting() {
+        App classUnderTest = new App();
+        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    }
+}
